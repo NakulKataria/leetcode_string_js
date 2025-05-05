bd53a75be42ea475b87178433936bfc25b9ff672
NakulKataria <nakulkofficial@gmail.com>
2025-05-05 17:20:30 +0000

    1021

---
+++ 1021. Remove Outermost Parentheses/solution.js
@@ -0,0 +1,29 @@
+/*A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+
+Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeOuterParentheses = function(s) {
+    let res = "";
+  let depth = 0;
+
+  for (let char of s) {
+      if (char === '(') {
+          if (depth > 0) res += char;
+          depth++;
+      } else if (char === ')') {
+          depth--;
+          if (depth > 0) res += char;
+      }
+  }
+
+  return res;
+};
